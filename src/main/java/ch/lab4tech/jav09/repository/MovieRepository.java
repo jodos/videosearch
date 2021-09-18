57c022d3963772fcf717ec9917c3fc6f3a906d69
Joao dos Santos <joao.dossantos@outlook.com>
2021-09-18 18:01:43 +0200

    Missed file

---
+++ src/main/java/ch/lab4tech/jav09/repository/MovieRepository.java
@@ -0,0 +1,8 @@
+package ch.lab4tech.jav09.repository;
+
+import ch.lab4tech.jav09.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Integer> {}
