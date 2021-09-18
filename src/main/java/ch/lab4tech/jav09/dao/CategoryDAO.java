4e9edb9415daa4d06b217bf0a80fa65f422d1453
Joao dos Santos <joao.dossantos@outlook.com>
2021-09-18 17:56:23 +0200

    Remove jdbc DAO + MySQL dump

--- src/main/java/ch/lab4tech/jav09/dao/CategoryDAO.java
+++
@@ -1,8 +0,0 @@
-package ch.lab4tech.jav09.dao;
-
-import java.util.List;
-
-public interface CategoryDAO {
-
-    List<String> find();
-}
--- src/main/java/ch/lab4tech/jav09/dao/RatingDAO.java
+++
@@ -1,9 +0,0 @@
-package ch.lab4tech.jav09.dao;
-
-import ch.lab4tech.jav09.model.Rating;
-
-import java.util.List;
-
-public interface RatingDAO {
-    List<Rating> find();
-}
