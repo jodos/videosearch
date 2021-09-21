662a01e185317267fe1ce7a813b43fb5bb7685b3
Joao dos Santos <joao.dossantos@outlook.com>
2021-09-21 19:57:12 +0200

    Fix Intellij IDEA warning

--- src/main/java/ch/lab4tech/jav09/gui/top/LeftTopPanel.java
+++ src/main/java/ch/lab4tech/jav09/gui/top/LeftTopPanel.java
@@ -14,49 +14,49 @@ import java.util.Vector;
 @Getter
 public class LeftTopPanel extends JPanel {
 
-  private final JLabel label;
-  private final JScrollPane scrollPane;
-  private final JList list;
+    private final JLabel label;
+    private final JScrollPane scrollPane;
+    private final JList<String> list;
 
-  public LeftTopPanel(MainGUI mainGUI, CategoryRepository categoryRepository) {
-    this.setPreferredSize(new Dimension(266, 200));
-    this.setLayout(new BorderLayout(0, 0));
-    label = new JLabel("Catégorie");
-    label.setHorizontalAlignment(SwingConstants.CENTER);
+    public LeftTopPanel(MainGUI mainGUI, CategoryRepository categoryRepository) {
+        this.setPreferredSize(new Dimension(266, 200));
+        this.setLayout(new BorderLayout(0, 0));
+        label = new JLabel("Catégorie");
+        label.setHorizontalAlignment(SwingConstants.CENTER);
 
-    final List<Category> categories = categoryRepository.findAll();
-    scrollPane = new JScrollPane();
-    list = new JList(new Vector<>(entityToString(categories)));
-    scrollPane.setViewportView(list);
-    list.addListSelectionListener(mainGUI);
+        final List<Category> categories = categoryRepository.findAll();
+        scrollPane = new JScrollPane();
+        list = new JList<>(new Vector<>(entityToString(categories)));
+        scrollPane.setViewportView(list);
+        list.addListSelectionListener(mainGUI);
 
-    this.add(label, BorderLayout.NORTH);
-    this.add(scrollPane, BorderLayout.CENTER);
-  }
-
-  public List<Category> getCurrentCategories() {
-    int[] indices = list.getSelectedIndices();
-    List<Category> categories = new ArrayList<>(indices.length);
-    for (int index : indices) {
-      Category category = new Category();
-      category.setCode(index + 1);
-      categories.add(category);
+        this.add(label, BorderLayout.NORTH);
+        this.add(scrollPane, BorderLayout.CENTER);
     }
-    return categories;
-  }
 
-  public void reset() {
-    list.clearSelection();
-  }
-
-
-  // TODO find a better way to convert List<Category> to List<String>
-  private List<String> entityToString(List<Category> categories) {
-    final List<String> l = new ArrayList<>(categories.size());
-
-    for (Category c : categories) {
-      l.add(c.getDescription());
+    public List<Category> getCurrentCategories() {
+        int[] indices = list.getSelectedIndices();
+        List<Category> categories = new ArrayList<>(indices.length);
+        for (int index : indices) {
+            Category category = new Category();
+            category.setCode(index + 1);
+            categories.add(category);
+        }
+        return categories;
     }
-    return l;
-  }
+
+    public void reset() {
+        list.clearSelection();
+    }
+
+
+    // TODO find a better way to convert List<Category> to List<String>
+    private List<String> entityToString(List<Category> categories) {
+        final List<String> l = new ArrayList<>(categories.size());
+
+        for (Category c : categories) {
+            l.add(c.getDescription());
+        }
+        return l;
+    }
 }
